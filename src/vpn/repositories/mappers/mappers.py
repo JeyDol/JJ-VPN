cf02710128672848000004a3ebb450a3f5e8ecbf
exl <levzavadskiy999@gmail.com>
2026-02-15 20:31:49 +0300

    feat: add mapper for peers

--- src/vpn/repositories/mappers/mappers.py
+++ src/vpn/repositories/mappers/mappers.py
@@ -1,5 +1,8 @@
+from src.vpn.db.models.peers import PeersOrm
 from src.vpn.db.models.users import UsersOrm
 from src.vpn.repositories.mappers.base import DataMapper
+from src.vpn.schemas.peers import PeerRead, PeerCreate, PeerUpdate
+from src.vpn.schemas.users import UserRead, UserCreate, UserUpdate
 
 
 class UsersDataMapper(DataMapper):
@@ -14,6 +17,23 @@ class UsersDataMapper(DataMapper):
     @classmethod
     def apply_update(cls, db_obj: UsersOrm, schema_obj: UserUpdate) -> UsersOrm:
         data = schema_obj.model_dump(exclude_unset=True, exclude_none=False)
+        for k, v in data.items():
+            setattr(db_obj, k, v)
+        return db_obj
+
+
+class PeersDataMapper(DataMapper):
+    db_model = PeersOrm
+    schema = PeerRead
+
+    @classmethod
+    def from_create(cls, schema_obj: PeerCreate) -> PeersOrm:
+        data = schema_obj.model_dump(exclude_unset=True, exclude_none=True)
+        return cls.db_model(**data)
+
+    @classmethod
+    def apply_update(cls, db_obj: PeersOrm, schema_obj: PeerUpdate) -> PeersOrm:
+        data = schema_obj.model_dump(exclude_unset=True, exclude_none=True)
         for k, v in data.items():
             setattr(db_obj, k, v)
         return db_obj
