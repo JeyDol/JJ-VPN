ed74147a6aadc48142c35b682d0df59d784d866b
exl <levzavadskiy999@gmail.com>
2026-02-19 00:45:44 +0300

    feat: add transaction_repo to dependencies

--- src/vpn/db/dependencies.py
+++ src/vpn/db/dependencies.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.vpn.db.base import AsyncSessionLocal, get_session
 from src.vpn.repositories.ip_pools import IPPoolRepository
 from src.vpn.repositories.peers import PeersRepository
+from src.vpn.repositories.transactions import TransactionsRepository
 from src.vpn.repositories.users import UsersRepository
 
 
@@ -18,3 +19,5 @@ async def get_ip_pool_repository(session: AsyncSession = Depends(get_session)) -
     return IPPoolRepository(session)
 
 
+async def get_transaction_repository(session: AsyncSession = Depends(get_session)) -> TransactionsRepository:
+    return TransactionsRepository(session)
