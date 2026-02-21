54a6ce3d36f9b74a7b717d6ab54a7f9599df2fac
exl <levzavadskiy999@gmail.com>
2026-02-21 17:17:22 +0300

    feat: new methods for user/peers service

--- src/vpn/db/dependencies.py
+++ src/vpn/db/dependencies.py
@@ -1,10 +1,11 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.vpn.db.base import AsyncSessionLocal, get_session
-from src.vpn.repositories.ip_pools import IPPoolRepository
+from src.vpn.db.base import get_session
 from src.vpn.repositories.peers import PeersRepository
 from src.vpn.repositories.transactions import TransactionsRepository
 from src.vpn.repositories.users import UsersRepository
+from src.vpn.services.peers import PeersService
+from src.vpn.services.users import UsersService
 
 
 async def get_user_repository(session: AsyncSession = Depends(get_session)) -> UsersRepository:
@@ -17,3 +18,25 @@ async def get_peer_repository(session: AsyncSession = Depends(get_session)) ->Pe
 
 async def get_transaction_repository(session: AsyncSession = Depends(get_session)) -> TransactionsRepository:
     return TransactionsRepository(session)
+
+
+async def get_user_service(
+        user_repo: UsersRepository = Depends(get_user_repository),
+        peer_repo: PeersRepository = Depends(get_peer_repository),
+        transaction_repo: TransactionsRepository = Depends(get_transaction_repository)
+)-> UsersService:
+    return UsersService(
+        user_repo=user_repo,
+        peer_repo=peer_repo,
+        trans_repo=transaction_repo
+    )
+
+
+async def get_peers_service(
+    peer_repo: PeersRepository = Depends(get_peer_repository),
+    user_service: UsersService = Depends(get_user_service)
+) -> PeersService:
+    return PeersService(
+        peer_repo=peer_repo,
+        user_serv=user_service
+    )
