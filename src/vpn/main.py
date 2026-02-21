5c6527ff665c42385f8fc51495af1382ac28f674
exl <levzavadskiy999@gmail.com>
2026-02-21 19:26:22 +0300

    feat: add routers

--- src/vpn/main.py
+++ src/vpn/main.py
@@ -1,5 +1,32 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.vpn.api.routes import peers, users
+from src.vpn.db.base import engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Приложение запущено")
+    yield
+    print("Приложение остановлено")
+    await engine.despose()
+
+
+app = FastAPI(
+    title="VPN Service",
+    version="0.1.0",
+    lifespan=lifespan
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "VPN Service API"}
+
+
+app.include_router(peers.router)
+app.include_router(users.router)
 
 
